45b08459a918dbba466e4086660da7f132fbc015
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-10 11:18:14 -0300

    PasswordEncoder

--- src/main/java/com/verzel/backend/BackendApplication.java
+++ src/main/java/com/verzel/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package com.verzel.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BackendApplication {
@@ -11,4 +14,8 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
