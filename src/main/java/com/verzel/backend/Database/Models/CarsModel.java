1a601e634fe3df044e8d1a4baa3b61d68d748eca
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-10 10:24:14 -0300

    :card_file_box: cars model

---
+++ src/main/java/com/verzel/backend/Database/Models/CarsModel.java
@@ -0,0 +1,26 @@
+package com.verzel.backend.Database.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "cars")
+public class CarsModel {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    private String name;
+    private String brand;
+    private String model;
+    private String description;
+    private String photo;
+}
