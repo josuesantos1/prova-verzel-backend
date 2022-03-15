b2206d2c72b1532f65bcb4d0822af9779a36d0fc
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-15 12:08:12 -0300

    :card_file_box: (cars:repository) findAllByOwner

--- src/main/java/com/verzel/backend/Database/Repositories/CarsRepository.java
+++ src/main/java/com/verzel/backend/Database/Repositories/CarsRepository.java
@@ -1,7 +1,9 @@
 package com.verzel.backend.Database.Repositories;
 
 import com.verzel.backend.Database.Models.CarsModel;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;;import java.util.List;
+import java.util.Optional;
 
 public interface CarsRepository extends JpaRepository<CarsModel, String> {
+    public Optional<List<CarsModel>> findAllByOwner(String owner);
 }
